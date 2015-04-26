7df7eaeb8ea308751757775ec249b9f84da249d6
Jakob Odersky <jodersky@gmail.com>
2015-04-26 15:44:16 +0200

    initial commit

---
+++ toggle-workspace@jodersky.github.com/extension.js
@@ -0,0 +1,39 @@
+
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Meta = imports.gi.Meta;
+const Shell = imports.gi.Shell;
+const Utils = imports.misc.extensionUtils.getCurrentExtension().imports.util; 
+
+let primary, saved;
+ 
+function _toggleWorkspace() {
+  let current = global.screen.get_active_workspace().index();
+  if (current == primary) {
+    global.screen.get_workspace_by_index(saved).activate(global.get_current_time());
+  } else {
+    global.screen.get_workspace_by_index(primary).activate(global.get_current_time());
+    saved = current;
+  }
+}
+
+function init() {
+  primary = 0;
+  saved = 1;
+}
+
+function enable() {
+  Main.wm.addKeybinding(
+    'toggle-primary-workspace',
+    Utils.getSettings(),
+    Meta.KeyBindingFlags.NONE,
+    Shell.KeyBindingMode.NORMAL | Shell.KeyBindingMode.MESSAGE_TRAY,
+    function() {
+      _toggleWorkspace();
+    }
+  );
+}
+
+function disable() {
+  Main.wm.removeKeybinding('toggle-primary-workspace');
+}
